de7f41d24ad36d5c3631590b2cc2292453c1d384
Hrach Tadevosyan <tadevosyanhrach@gmail.com>
2016-07-12 04:13:05 +0400

    add vue validator on login

--- resources/assets/js/components/login.vue
+++ resources/assets/js/components/login.vue
@@ -3,68 +3,104 @@
 </style>
 
 <template>
-    <form class="login-form" action="" method="post">
-        <h3 class="form-title">Login to your account</h3>
-        <div class="alert alert-danger display-hide">
-            <button class="close" data-close="alert"></button>
-            <span> Enter any username and password. </span>
-        </div>
-        <div class="form-group">
-            <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-            <label class="control-label visible-ie8 visible-ie9">Username</label>
-            <div class="input-icon">
-                <i class="fa fa-user"></i>
-                <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username" /> </div>
-        </div>
-        <div class="form-group">
-            <label class="control-label visible-ie8 visible-ie9">Password</label>
-            <div class="input-icon">
-                <i class="fa fa-lock"></i>
-                <input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password" /> </div>
-        </div>
-        <div class="form-actions">
-            <label class="rememberme mt-checkbox mt-checkbox-outline">
-                <input type="checkbox" name="remember" value="1" /> Remember me
-                <span></span>
-            </label>
-            <button type="submit" class="btn green pull-right"> Login </button>
-        </div>
-        <div class="login-options">
-            <h4>Or login with</h4>
-            <ul class="social-icons">
-                <li>
-                    <a class="facebook" data-original-title="facebook" href="javascript:;"> </a>
-                </li>
-                <li>
-                    <a class="twitter" data-original-title="Twitter" href="javascript:;"> </a>
-                </li>
-                <li>
-                    <a class="googleplus" data-original-title="Goole Plus" href="javascript:;"> </a>
-                </li>
-                <li>
-                    <a class="linkedin" data-original-title="Linkedin" href="javascript:;"> </a>
-                </li>
-            </ul>
-        </div>
-        <div class="forget-password">
-            <h4>Forgot your password ?</h4>
-            <p> no worries, click
-                <a v-link="{ path: '/forgot' }" id="forget-password"> here </a> to reset your password. </p>
-        </div>
-        <div class="create-account">
-            <p> Don't have an account yet ?&nbsp;
-                <a v-link="{ path: '/register' }" id="register-btn"> Create an account </a>
-            </p>
-        </div>
-    </form>
+    <validator name="loginValidation">
+        <form class="login-form" method="post" novalidate v-on:submit.prevent="login">
+            <h3 class="form-title">Login to your account</h3>
+            <div class="alert alert-danger display-hide">
+                <button class="close" data-close="alert"></button>
+                <span> Enter any username and password. </span>
+            </div>
+            <div class="form-group" v-bind:class="{'has-error' : $loginValidation.username.required}">
+                <label class="control-label visible-ie8 visible-ie9">Username</label>
+                <div class="input-icon">
+                    <i class="fa fa-user"></i>
+                    <input class="form-control placeholder-no-fix"
+                           type="text"
+                           autocomplete="off"
+                           placeholder="Username"
+                           name="username"
+                           v-validate:username="['required']"
+                    />
+                    <span class="help-block"
+                          v-if="$loginValidation.username.required"
+                    >Username is required</span>
+                </div>
+            </div>
+            <div class="form-group"
+                 v-bind:class="{'has-error' : $loginValidation.password.minlength}"
+            >
+                <label class="control-label visible-ie8 visible-ie9">Password</label>
+                <div class="input-icon">
+                    <i class="fa fa-lock"></i>
+                    <input class="form-control placeholder-no-fix"
+                           type="password"
+                           autocomplete="off"
+                           placeholder="Password"
+                           name="password"
+                           v-validate:password="{required : true,minlength : 4}"
+                    />
+                    <span class="help-block"
+                          v-if="$loginValidation.password.required"
+                    >Password is required</span>
+                    <span class="help-block"
+                          v-if="$loginValidation.password.minlength"
+                    >Password min length is 4</span>
+                </div>
+            </div>
+            <div class="form-actions">
+                <label class="rememberme mt-checkbox mt-checkbox-outline">
+                    <input type="checkbox" name="remember" value="1" /> Remember me
+                    <span></span>
+                </label>
+                <button type="submit"
+                        class="btn green pull-right"
+                        v-if="$loginValidation.valid"
+                > Login </button>
+            </div>
+            <div class="login-options">
+                <h4>Or login with</h4>
+                <ul class="social-icons">
+                    <li>
+                        <a class="facebook" data-original-title="facebook" href="javascript:;"> </a>
+                    </li>
+                    <li>
+                        <a class="twitter" data-original-title="Twitter" href="javascript:;"> </a>
+                    </li>
+                    <li>
+                        <a class="googleplus" data-original-title="Goole Plus" href="javascript:;"> </a>
+                    </li>
+                    <li>
+                        <a class="linkedin" data-original-title="Linkedin" href="javascript:;"> </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="forget-password">
+                <h4>Forgot your password ?</h4>
+                <p> no worries, click
+                    <a v-link="{ path: '/forgot' }" id="forget-password"> here </a> to reset your password. </p>
+            </div>
+            <div class="create-account">
+                <p> Don't have an account yet ?&nbsp;
+                    <a v-link="{ path: '/register' }" id="register-btn"> Create an account </a>
+                </p>
+            </div>
+        </form>
+    </validator>
 </template>
 
 <script>
     import Vue from 'vue';
     import VueResource from 'vue-resource';
+    import VueValidator from 'vue-validator';
 
     Vue.use(VueResource);
+    Vue.use(VueValidator);
     export default Vue.extend({
 
+        methods : {
+            login(){
+//                if(this.$loginValidation.valid)
+            }
+        }
     })
 </script>
